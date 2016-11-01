8a103fd9abe9a28554bc32bf92a6d2de6762f230
oakraw <oakraw@gmail.com>
2016-11-01 17:37:23 +0700

    Use LocationManager instead

--- app/src/main/java/com/whileplex/pipproject/BackgroundService.java
+++ app/src/main/java/com/whileplex/pipproject/BackgroundService.java
@@ -7,20 +7,18 @@ import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import io.nlopez.smartlocation.OnLocationUpdatedListener;
 import io.nlopez.smartlocation.SmartLocation;
@@ -28,21 +26,18 @@ import io.nlopez.smartlocation.SmartLocation;
 /**
  * Created by Rawipol on 8/30/15 AD.
  */
-public class BackgroundService extends Service {
+public class BackgroundService extends Service implements LocationListener{
 
-    public static int update_time = 0;
-    private Location currentLocation;
+    private static int updateTime = 1;
     protected NotificationManager notificationManager;
     private static final int NOTI_ID = 123801;
-    private Firebase myFirebaseRef;
     private String userID;
+    private LocationManager locationManager;
+    private String provider;
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Firebase.setAndroidContext(this);
-        userID = intent.getStringExtra(AppController.UIDTAG);
-
         return Service.START_STICKY;
     }
 
@@ -58,82 +53,72 @@ public class BackgroundService extends Service {
         super.onCreate();
 
         Toast.makeText(this, "Start tracking", Toast.LENGTH_SHORT).show();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        locationManager.requestLocationUpdates(provider, 5000, 1, this);
 
 
-        myFirebaseRef = new Firebase("https://whileplex-pip.firebaseio.com/");
-        createNotification(0,0, "Updating...");
-        SmartLocation.with(getApplication()).location()
-                .start(new OnLocationUpdatedListener() {
-                           @Override
-                           public void onLocationUpdated(Location location) {
-                               currentLocation = location;
-                               sendData(location.getLatitude(), location.getLongitude());
-                               update_time++;
-                           }
-                       }
-                );
-
+        createNotification(0, 0, "Updating...");
+        if (location != null) {
+            onLocationChanged(location);
+        }
     }
 
+    private String getTime(){
+        Date date = Calendar.getInstance().getTime();
+        return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    }
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         super.onDestroy();
         Toast.makeText(getApplication(), "Stop tracking", Toast.LENGTH_LONG).show();
-        if(notificationManager != null){
+        if (notificationManager != null) {
             notificationManager.cancel(NOTI_ID);
         }
-        SmartLocation.with(getApplication()).location().stop();
+        locationManager.removeUpdates(this);
     }
 
-    private void sendData(double lat, double lng){
-
-        if(userID == null){
-            userID = AppController.uid;
-        }
-
-        if(userID != null) {
-
-            myFirebaseRef.child("users").child(userID).child("latestUpdate").setValue(System.currentTimeMillis() / 1000L);
-            myFirebaseRef.child("users").child(userID).child("lat").setValue(lat);
-            myFirebaseRef.child("users").child(userID).child("lng").setValue(lng, new Firebase.CompletionListener() {
-                @Override
-                public void onComplete(FirebaseError firebaseError, Firebase firebase) {
-                    if(firebaseError == null)
-                        createNotification(currentLocation.getLatitude(), currentLocation.getLongitude(), "Update:"+update_time+" "+"Update successful");
-                    else
-                        createNotification(currentLocation.getLatitude(), currentLocation.getLongitude(), "Error: please re-login and try again.");
-
-                }
-
-
-            });
-
-        }
-    }
-
-    public void createNotification(double lat, double lng,String message) {
-        // Prepare intent which is triggered if the
-        // notification is selected
+    public void createNotification(double lat, double lng, String message) {
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
 
         Intent cancelIntent = new Intent(this, CancelButtonListener.class);
         PendingIntent cancelPIntent = PendingIntent.getBroadcast(this, (int) System.currentTimeMillis(), cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        // Build notification
-        // Actions are just fake
         Notification noti = new Notification.Builder(this)
-                .setContentTitle("Lat:"+lat+" Lng:"+lng)
-                .setContentText(message).setSmallIcon(R.drawable.female188)
+                .setContentTitle("Lat:" + lat + " Lng:" + lng)
+                .setContentText(message)
+                .setSmallIcon(R.drawable.female188)
                 .setAutoCancel(false)
                 .setPriority(Notification.PRIORITY_MAX)
                 .addAction(R.drawable.ic_cancel_white_24dp, "Stop tracking", cancelPIntent)
                 .setContentIntent(pIntent).build();
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        // hide the notification after its selected
         noti.flags = Notification.FLAG_ONGOING_EVENT;
         notificationManager.notify(NOTI_ID, noti);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updateTime += 1;
+        createNotification(location.getLatitude(), location.getLongitude(), "Updated at " + getTime());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 
@@ -148,5 +133,8 @@ public class BackgroundService extends Service {
         }
     }
 
+    public static int getUpdateTime() {
+        return updateTime;
+    }
 }
 
--- app/src/main/java/com/whileplex/pipproject/MainActivity.java
+++ app/src/main/java/com/whileplex/pipproject/MainActivity.java
@@ -3,170 +3,83 @@ package com.whileplex.pipproject;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.firebase.client.AuthData;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-//import com.firebase.client.Firebase;
-
 import io.nlopez.smartlocation.OnLocationUpdatedListener;
 import io.nlopez.smartlocation.SmartLocation;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, LocationListener {
 
     private TextView latTv;
     private TextView lngTv;
     private TextView updateTime;
     private Button startBtn;
-    private Button stopBtn;
-    private Button resetBtn;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        latTv = (TextView)findViewById(R.id.lat_tv);
-        lngTv = (TextView)findViewById(R.id.lng_tv);
-        updateTime = (TextView)findViewById(R.id.update_time);
-        startBtn = (Button)findViewById(R.id.start_service_btn);
-        stopBtn = (Button)findViewById(R.id.stop_service_btn);
-        resetBtn = (Button)findViewById(R.id.reset_btn);
+        latTv = (TextView) findViewById(R.id.lat_tv);
+        lngTv = (TextView) findViewById(R.id.lng_tv);
+        updateTime = (TextView) findViewById(R.id.update_time);
+        startBtn = (Button) findViewById(R.id.start_service_btn);
 
         startBtn.setOnClickListener(this);
-        stopBtn.setOnClickListener(this);
-        resetBtn.setOnClickListener(this);
+        updateTime.setVisibility(View.INVISIBLE);
 
-        stopBtn.setVisibility(View.GONE);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
 
-
-        SmartLocation.with(this).location()
-                .start(new OnLocationUpdatedListener() {
-                    @Override
-                    public void onLocationUpdated(Location location) {
-                        latTv.setText("Lat: "+location.getLatitude());
-                        lngTv.setText("Lng: "+location.getLongitude());
-                        updateTime.setText(BackgroundService.update_time+"");
-                    }
-                });
+        if(location != null){
+            onLocationChanged(location);
+        }
 
     }
 
-    private void resetData(){
-        AppController.getFirebase().child("users").child(AppController.uid).child("lat").setValue(0);
-        AppController.getFirebase().child("users").child(AppController.uid).child("lng").setValue(0, new Firebase.CompletionListener() {
-            @Override
-            public void onComplete(FirebaseError firebaseError, Firebase firebase) {
-                if(firebaseError == null)
-                    Toast.makeText(MainActivity.this, "Reset successful", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.start_service_btn:
+                Toast.makeText(this, "Start tracking", Toast.LENGTH_SHORT).show();
                 Intent bgService = new Intent(getApplicationContext(), BackgroundService.class);
-                bgService.putExtra(AppController.UIDTAG,AppController.uid);
                 startService(bgService);
-                Log.d("oakTag",AppController.uid);
-                break;
-            case R.id.stop_service_btn:
-                Toast.makeText(this, "stop tracking", Toast.LENGTH_SHORT).show();
-                stopService(new Intent(getApplicationContext(), BackgroundService.class));
-                break;
-            case R.id.reset_btn:
-                resetData();
                 break;
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.logout) {
-            AppController.getFirebase().unauth();
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-
-    /*check internet connection*/
-    public boolean hasConnection() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(
-                Context.CONNECTIVITY_SERVICE);
-
-        NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (wifiNetwork != null && wifiNetwork.isConnected()) {
-            return true;
-        }
-
-        NetworkInfo mobileNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if (mobileNetwork != null && mobileNetwork.isConnected()) {
-            return true;
-        }
-
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        if (activeNetwork != null && activeNetwork.isConnected()) {
-            return true;
-        }
-
-        return false;
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(hasConnection()){
-            if (!((LocationManager)getSystemService(Context.LOCATION_SERVICE)).isProviderEnabled(LocationManager.GPS_PROVIDER)){
-                LocationAlertDialog();
-            }
+        if (!((LocationManager) getSystemService(Context.LOCATION_SERVICE)).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            LocationAlertDialog();
+        }else{
+            locationManager.requestLocationUpdates("gps", 5000, 1, this);
         }
-        else{
-            Toast.makeText(this, "Please connect internet.", Toast.LENGTH_SHORT).show();
-        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
     }
 
     /*show dialog to enable GPS*/
@@ -195,5 +108,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         d.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latTv.setText("Lat: " + location.getLatitude());
+        lngTv.setText("Lng: " + location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d(getClass().getName(), "onStatusChanged() called with: provider = [" + provider + "], status = [" + status + "], extras = [" + extras + "]");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
